14d0633b65d0ced7b41e213e1deb11a05aefe034
Goran Lojanica <goran.lojanica5@gmail.com>
2021-01-29 01:39:59 +0100

    initial-project-base

---
+++ isa-pharmacy/src/main/java/internet/software/architectures/team31/isapharmacy/IsaPharmacyApplication.java
@@ -0,0 +1,13 @@
+package internet.software.architectures.team31.isapharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaPharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaPharmacyApplication.class, args);
+	}
+
+}
---
+++ isa-pharmacy/src/test/java/internet/software/architectures/team31/isapharmacy/IsaPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package internet.software.architectures.team31.isapharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsaPharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
