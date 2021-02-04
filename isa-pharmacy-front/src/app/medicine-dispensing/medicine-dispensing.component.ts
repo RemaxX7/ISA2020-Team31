76ed3132e619c1914d200d7149fa6bc565a77c6d
RemaxX7 <marko.rakic98@gmail.com>
2021-02-04 20:52:44 +0000

    test values for textareas,patient search update

--- isa-pharmacy-front/src/app/medicine-dispensing/medicine-dispensing.component.ts
+++ isa-pharmacy-front/src/app/medicine-dispensing/medicine-dispensing.component.ts
@@ -10,6 +10,37 @@ export class MedicineDispensingComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.HideTable();
   }
-
+  HideTable(){
+    var table = document.getElementById("myTable");
+    table.style.display = "none";
+  }
+  ShowTable(){
+    var table = document.getElementById("myTable");
+    table.style.display = "";
+  }
+  MyFunction(){
+    var input, filter, table, tr, i,td1;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    let novi:string = filter;
+    if(novi==''){
+      this.HideTable();
+    }else{
+      this.ShowTable();
+    }
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td1 = tr[i].getElementsByTagName("td")[2];
+        if(td1){
+        if (td1.innerHTML.toUpperCase().indexOf(filter) > -1 ) {
+            tr[i].style.display = "";
+        } else {
+            tr[i].style.display = "none";
+        } 
+    }
+  }
+}
 }
